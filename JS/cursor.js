4d3bfe60d6ec60d06df2ab6077b7f4d63ade06e5
Crimeocean <119916890+Crimeocean@users.noreply.github.com>
2023-01-31 12:18:12 -0800

    Update cursor.js

--- JS/cursor.js
+++ JS/cursor.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
                 });
             });
     
-        $(".link")
+        $(".link, .logolink")
             .mouseenter(function() {
                 cursor.css({
                     transform: "scale(3.2)"
